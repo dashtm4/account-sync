9fcf7587148e4151c35086681f68bdca388b3da8
Nikita <nikita@MacBook-Pro-2.local>
2020-07-16 11:12:27 +0300

    Make signUp protected

--- src/lambdas/signUp.ts
+++ src/lambdas/signUp.ts
@@ -2,7 +2,7 @@ import AWS from 'aws-sdk';
 import middy from 'middy';
 import { jsonBodyParser } from 'middy/middlewares';
 import Boom from '@hapi/boom';
-import { APIGatewayEvent, SignUpEvent } from '../types/aws';
+import { APIGatewayEvent } from '../types/aws';
 import { APIGatewayResponse } from '../utils/aws';
 import { apiGatewayResponse } from '../middlewares/apiGateWayResponse';
 
@@ -12,16 +12,17 @@ interface SignUpResponse {
     message: string;
 }
 
-const rawHandler = async (event: APIGatewayEvent<SignUpEvent>)
+const rawHandler = async (event: APIGatewayEvent<null>)
 : Promise<APIGatewayResponse<SignUpResponse>> => {
     // eslint-disable-next-line no-console
     console.log(event);
-    if (!event.body) return Boom.badData('Body was not provided');
+
+    const cognito = event.requestContext.authorizer.claims;
 
     try {
         const { Item } = await dynamoDb.get({
             TableName: process.env.tableName!,
-            Key: { Email: event.body.email },
+            Key: { Email: cognito.email },
         }).promise();
         if (Item) {
             throw Boom.notAcceptable('User already exists');
@@ -33,8 +34,8 @@ const rawHandler = async (event: APIGatewayEvent<SignUpEvent>)
     const params = {
         TableName: process.env.tableName!,
         Item: {
-            Email: event.body.email,
-            CognitoId: event.body.cognitoId,
+            Email: cognito.email,
+            CognitoId: cognito.sub,
         },
     };
     try {
@@ -48,4 +49,4 @@ const rawHandler = async (event: APIGatewayEvent<SignUpEvent>)
 
 export const handler = middy(rawHandler)
     .use(jsonBodyParser())
-    .use(apiGatewayResponse<APIGatewayEvent<SignUpEvent>, APIGatewayResponse<SignUpResponse>>());
+    .use(apiGatewayResponse<APIGatewayEvent<null>, APIGatewayResponse<SignUpResponse>>());
