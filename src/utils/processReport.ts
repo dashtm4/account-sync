75cb0752b6d63d0250202af8c0247cc2fff6afbb
Scott Feinberg <scott@devpassport.com>
2020-11-01 10:28:37 -0500

    remove debug

--- src/utils/processReport.ts
+++ src/utils/processReport.ts
@@ -29,12 +29,8 @@ const processUltraTax = (accounts: AWS.DynamoDB.DocumentClient.ItemList) => {
     }
     for (const account of accounts) {
         if (account.TaxCode){
-            console.log("before tax code");
             const taxCode = account.TaxCode.withIndent();
-            console.log("before AcctNum");
-            console.log(JSON.stringify(account));
             const acctNum = account.AcctNum ? account.AcctNum.toString().withIndent() : account.AccountName.withIndent();
-            console.log("afteracctnum");
             const value = account.ValueCents ? flipSign(account.Toggle, account.ValueCents) : '';
             const name = account.AccountName.substring(0, 29);
             data.push(`${taxCode}${acctNum}${new Array(longest + 10 - value.toString().length).join(' ')}${value.toString()}${new Array(10).join(' ')}${name + new Array(100 - name.length).join(' ')}`);
