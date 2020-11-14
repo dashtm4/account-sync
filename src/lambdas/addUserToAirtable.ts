15b2222ab02150cf410a40c3baec887a2f0e7fcd
Scott Feinberg <scott@devpassport.com>
2020-11-13 19:21:18 -0500

    change how it passes promise

--- src/lambdas/addUserToAirtable.ts
+++ src/lambdas/addUserToAirtable.ts
@@ -1,25 +1,32 @@
 
 export const handler = async (event: any)
 : Promise<any> => {
+    
+    console.log(JSON.stringify(event));
     var Airtable = require('airtable');
     Airtable.configure({
         endpointUrl: 'https://api.airtable.com',
         apiKey: process.env.AIRTABLE_API_KEY
     });
     var base = Airtable.base('appSgOJ4dAfje507d');
-    console.log(JSON.stringify(event));
-    return new Promise(base('Contacts').create({
-        "Phone #": event.detail.Item.OfficePhoneNumber,
-        "Name": event.detail.Item.Name,
-        "Email Address": event.detail.Item.Email
-    }, function (err: any, record: any) {
-        console.log("callback executing");
-        if (err) {
-            console.log("Error creating record");
-            console.error(err);
-            return;
-        }
-        console.log(record.getId());
-    }));
+
+    var p = new Promise((resolve, reject) => {
+        resolve(base('Contacts').create({
+            "Phone #": event.detail.Item.OfficePhoneNumber,
+            "Name": event.detail.Item.Name,
+            "Email Address": event.detail.Item.Email
+        }, function (err: any, record: any) {
+            console.log("callback executing");
+            if (err) {
+                console.log("Error creating record");
+                console.error(err);
+                return;
+            }
+            console.log(record.getId());
+        }));
+    });
+
+    return p;
 };
+
 
