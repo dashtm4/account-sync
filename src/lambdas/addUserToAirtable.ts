f0b6c04843201dd65e71cbf1c20c0e0bf87d14de
Scott Feinberg <scott@devpassport.com>
2020-11-13 19:49:30 -0500

    turn off for production

--- src/lambdas/addUserToAirtable.ts
+++ src/lambdas/addUserToAirtable.ts
@@ -1,27 +1,25 @@
 
 export const handler = function(event: any, context: any, callback: any) {
-    console.log(JSON.stringify(event));
-    var Airtable = require('airtable');
-    Airtable.configure({
-        endpointUrl: 'https://api.airtable.com',
-        apiKey: process.env.AIRTABLE_API_KEY
-    });
-    var base = Airtable.base('appSgOJ4dAfje507d');
-    base('Contacts').create({
-        "Phone #": event.detail.Item.OfficePhoneNumber,
-        "Name": event.detail.Item.Name,
-        "Email Address": event.detail.Item.Email
-    }, function (err: any, record: any) {
-        console.log("callback executing");
-        if (err) {
-            console.log("Error creating record");
-            console.error(err);
-            return callback(Error(err));
-        }else{
-            console.log(record.getId());
-            return callback(null, 200);
-        }
-    });
+    if (process.env.environment == "production"){
+        var Airtable = require('airtable');
+        Airtable.configure({
+            endpointUrl: 'https://api.airtable.com',
+            apiKey: process.env.AIRTABLE_API_KEY
+        });
+        var base = Airtable.base('appSgOJ4dAfje507d');
+        base('Contacts').create({
+            "Phone #": event.detail.Item.OfficePhoneNumber,
+            "Name": event.detail.Item.Name,
+            "Email Address": event.detail.Item.Email
+        }, function (err: any, record: any) {
+            if (err) {
+                console.error(err);
+                return callback(Error(err));
+            }else{
+                return callback(null, 200);
+            }
+        });
+    };
 };
 
 
